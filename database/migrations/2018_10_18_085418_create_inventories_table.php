496900f5484aade680ae53b11a6b67216148c4ee
sohamdigheoss <sohamdigheoss@gmail.com>
2018-10-18 15:45:36 +0530

    intial commit

---
+++ database/migrations/2018_10_18_085418_create_inventories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('item_id');
+            $table->string('name');
+            $table->integer('quantity');
+            $table->string('vendor');
+            $table->string('category');
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
